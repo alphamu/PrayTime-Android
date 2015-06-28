03acac28322d5f61aea48c7c9e4d49dc3aede1af
Ali Muzaffar <ali@muzaffar.me>
2015-06-29 08:18:20 +1000

    Add a screen asking use  to configure the app before using it. Changed styles to make everything more theme controlled.

--- app/src/main/java/com/alimuzaffar/ramadanalarm/SalaatTimesActivity.java
+++ app/src/main/java/com/alimuzaffar/ramadanalarm/SalaatTimesActivity.java
@@ -5,18 +5,24 @@ import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import java.util.LinkedHashMap;
 import java.util.TimeZone;
 
 
-public class SalaatTimesActivity extends AppCompatActivity {
+public class SalaatTimesActivity extends AppCompatActivity implements View.OnClickListener {
 
   public static final String EXTRA_ALARM_INDEX = "alarm_index";
+
+  ViewGroup mTimesContainer;
+  TextView mConfigureNow;
+  TextView mUseDefault;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -26,20 +32,30 @@ public class SalaatTimesActivity extends AppCompatActivity {
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
 
-    if (!AppSettings.getInstance(this).getBoolean(AppSettings.Key.HAS_DEFAULT_SET)) {
-      Intent intent = new Intent(this, OnboardingActivity.class);
-      intent.putExtra(OnboardingActivity.EXTRA_CARD_INDEX, 0);
-      startActivity(intent);
+    LayoutInflater inflater = LayoutInflater.from(this);
+    mTimesContainer = (ViewGroup) findViewById(R.id.times_container);
 
+    if (!AppSettings.getInstance(this).getBoolean(AppSettings.Key.HAS_DEFAULT_SET)) {
+      mTimesContainer.removeAllViews();
+      View configure = inflater.inflate(R.layout.view_configure_app, mTimesContainer, true);
+      mConfigureNow = (TextView) configure.findViewById(R.id.configure_now);
+      mUseDefault = (TextView) configure.findViewById(R.id.use_default);
+
+      mConfigureNow.setOnClickListener(this);
+      mUseDefault.setOnClickListener(this);
+
+    } else {
+      init();
     }
 
 
+  }
+
+  private void init() {
     // In future releases we will add more cards.
     // Then we'll need to do this for each card.
     // For now it's included in the layout which
     // makes it easier to work with the layout editor.
-    // ViewGroup timesContainer = (ViewGroup) findViewById(R.id.times_container);
-    // LayoutInflater inflater = LayoutInflater.from(this);
     // inflater.inflate(R.layout.prayer_times, timesContainer, true);
 
     //Toolbar will now take on default Action Bar characteristics
@@ -68,7 +84,6 @@ public class SalaatTimesActivity extends AppCompatActivity {
     //set text for the first card.
     setAlarmButtonText(alarm, 0);
     setAlarmButtonClickListener(alarm, 0);
-
   }
 
   @Override
@@ -87,6 +102,7 @@ public class SalaatTimesActivity extends AppCompatActivity {
 
     //noinspection SimplifiableIfStatement
     if (id == R.id.action_settings) {
+      startOnboardingFor(0);
       return true;
     }
 
@@ -122,4 +138,22 @@ public class SalaatTimesActivity extends AppCompatActivity {
     }.init(index));
   }
 
+  @Override
+  public void onClick(View v) {
+    if (v.getId() == mConfigureNow.getId()) {
+      startOnboardingFor(0);
+
+    } else if (v.getId() == mUseDefault.getId()) {
+      mTimesContainer.removeAllViews();
+      LayoutInflater inflater = LayoutInflater.from(this);
+      inflater.inflate(R.layout.prayer_times, mTimesContainer);
+      init();
+    }
+  }
+
+  private void startOnboardingFor(int index) {
+    Intent intent = new Intent(getApplicationContext(), OnboardingActivity.class);
+    intent.putExtra(OnboardingActivity.EXTRA_CARD_INDEX, index);
+    startActivity(intent);
+  }
 }
